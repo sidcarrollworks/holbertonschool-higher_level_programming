751a8f7420fe3b2361a5cd2f2633ed7b3bc602cd
squidcarroll <squidcarroll@gmail.com>
2017-12-04 21:53:04 +0000

    number 10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,8 +1,27 @@
 #include "lists.h"
 /**
- *
- *
- *
- *
+ * check_cycle - check for a loop in linked list
+ * @list: head of list
+ * Return: 0 if no cycle and 1 if cycle
  */
-int check_cycle(listint_t *list
+int check_cycle(listint_t *list)
+{
+        listint_t *current = list;
+        listint_t *check = NULL;
+        int since = 0;
+        int sinceScale = 2;
+
+        do
+        {
+                if (check == current)
+                        return (1);
+                if (since >= sinceScale)
+                {
+                        check = current;
+                        since = 0;
+                        sinceScale = 2 * sinceScale;
+                }
+                since++;
+        } while ((current = current->next) != NULL);
+        return (0);
+}
