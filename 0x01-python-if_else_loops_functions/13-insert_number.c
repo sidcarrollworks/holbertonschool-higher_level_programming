5c7059ce61a1fa83f226f5878fb4c67596e60f85
squidcarroll <squidcarroll@gmail.com>
2017-12-06 07:11:52 +0000

    new 13

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,96 +1,45 @@
 #include "lists.h"
-listint_t *add_nodeint(listint_t **head, const int n);
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 /**
- * insert_node - inserts node into sorted linked list
- * @head: head of list
- * @number: data of new node
- * Return: returns pointer to node
+ * insert_node - inserts node in sorted list
+ * @head: head node
+ * @number: number to be inserted
+ * Return: new head node
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-        listint_t *tmp = NULL;
-        listint_t *tmp2 = NULL;
-        listint_t *new;
-        int i = 0;
-
-        tmp = *head;
-        tmp2 = tmp++
-        if (tmp < tmp++)
-        {
-                if (tmp->n < number)
-                {
-                        tmp = *head
-                        while (tmp->n < number)
-                                tmp++, i++;
-                        new = insert_nodeint_at_index(**head, i, number);
-                }
-                else
-                        new = add_nodeint(**head, number);
-        }
-        return (*new);
-}
-
-/**
- * add_nodeint - adds a node at the beginnint of the list
- * @head: linked list
- * @n: data we ant
- * Return: the address of the new element
- */
-listint_t *add_nodeint(listint_t **head, const int n)
-{
-        listint_t *new_node;
-
-        new_node = malloc(sizeof(listint_t));
-        if (!new_node)
-                return (NULL);
-
-        new_node->n = n;
-        new_node->next = (*head);
-        (*head) = new_node;
-        return (new_node);
-}
-
-/**
- * insert_nodeint_at_index - insert node at index
- * @head: linked list
- * @idx: posisiton to insert
- * @n: data
- * Return: address of new node or null if failed
- */
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
-{
-        listint_t *new_node;
+        int i = 0, f = 0;
         listint_t *tmp;
-        listint_t *tmp2;
-        listint_t *len;
-        unsigned int length = 0, i = 0;
-
-        len = *head;
-        while (len)
-        {
-                length++;
-                len = len->next;
-        }
-
-        if (idx > length)
-                return (NULL);
+        listint_t *new_node;
 
         new_node = malloc(sizeof(listint_t));
-        if (!new_node)
+        if (new_node == NULL)
                 return (NULL);
-        new_node->n = n;
-        new_node->next = NULL;
-        tmp = *head;
 
-        while (tmp && i < idx - 1)
+        tmp = *head;
+        while (tmp != NULL)
         {
+                if (i == 0 && tmp->n >= number)
+                {
+                        f++;
+                        break;
+                }
+                if (tmp->n <= number && (tmp->next == NULL || tmp->next->n >= number))
+                        break;
                 tmp = tmp->next;
                 i++;
         }
-        tmp2 = tmp;
-        tmp = new_node;
-        new_node = tmp2;
-        return (tmp2);
+
+        tmp = *head;
+        while (i != 0)
+                tmp = tmp->next, i--;
+        
+        new_node->n = number;
+        if (*head == NULL)
+                new_node->next = NULL, *head = new_node;
+        else if (f)
+                new_node->next = tmp, *head = new_node;
+        else
+                new_node->next = tmp->next, tmp->next = new_node;
+        return (*head);
 }
