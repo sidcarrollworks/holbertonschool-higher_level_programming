e5df959cb16033c65e296f59bc4358e0cf5eb283
squidcarroll <squidcarroll@gmail.com>
2017-12-06 05:41:58 +0000

    udpate 9

--- 0x01-python-if_else_loops_functions/3-alphabt.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-for x in range(97, 123):
-    if x != 101 and x != 113:
-        print(chr(x), end="")
--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    return (number % 10)
+    last = ''
+    if number < 0:
+        number = -number
+    last = number % 10
+    return last
