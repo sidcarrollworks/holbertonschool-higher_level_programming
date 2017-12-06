dbfcd44b2f3cf74b05f38847798c1f001aa0b69d
squidcarroll <squidcarroll@gmail.com>
2017-12-06 06:54:24 +0000

    fix 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
-while i
+for x in range(0, 10):
+    for i in range(0, 10):
+        if (x < i):
+            print('{}{}'.format(x, i), end='')
+            if (x < 8):
+                print(', ', end='')
+print('\n', end='')
