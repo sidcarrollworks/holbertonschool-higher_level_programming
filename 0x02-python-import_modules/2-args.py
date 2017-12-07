83d910792cc4d4817fd0e386aa59b78977f17d41
squidcarroll <squidcarroll@gmail.com>
2017-12-07 21:49:01 +0000

    fix for 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 from sys import argv
 if __name__ == "__main__":
-    print("{} arguements{}".format(len(argv) - 1, ":" if len(argv) > 1 else "."))
-    i = 0
+    if len(argv) > 1:
+        a = ":"
+    else:
+        a = "."
+    print("{} arguements{}".format(len(argv) - 1, a))
+    i = 1
     for j in argv[1:]:
         print("{}: {}".format(i, j))
         i += 1
