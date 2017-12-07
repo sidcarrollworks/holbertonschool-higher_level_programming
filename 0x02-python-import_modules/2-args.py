a9f27b5831f5e4edf3d900f4f6acdd75ecc5da8d
squidcarroll <squidcarroll@gmail.com>
2017-12-07 23:15:53 +0000

    final update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,10 +2,10 @@
 from sys import argv
 if __name__ == "__main__":
     if len(argv) == 2:
-        a = "argument."
+        a = "argument:"
     elif len(argv) == 1:
-        a = "arguments:"
-    elif len(argv) > 1:
+        a = "arguments."
+    else:
         a = "arguments:"
     print("{} {}".format(len(argv) - 1, a))
     i = 1
