819f8409e3ea4ff39a599adbba338417a7e1d4c0
squidcarroll <squidcarroll@gmail.com>
2017-12-07 20:56:52 +0000

    number 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 from sys import argv
-print("{} arguements{}".format(len(argv) - 1, ":" if len(argv) > 1 else "."))
-i = 0
-for j in argv[1:]:
-    print("{}: {}".format(i, j))
-    i += 1
+if __name__ == "__main__":
+    print("{} arguements{}".format(len(argv) - 1, ":" if len(argv) > 1 else "."))
+    i = 0
+    for j in argv[1:]:
+        print("{}: {}".format(i, j))
+        i += 1
