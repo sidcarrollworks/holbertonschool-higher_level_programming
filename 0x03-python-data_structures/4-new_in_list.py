478994e88736ed6b824911b627b573054978b046
squidcarroll <squidcarroll@gmail.com>
2017-12-12 23:17:15 +0000

    fix for 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@ def new_in_list(my_list, idx, element):
     listcpy = my_list.copy()
     if idx < 0:
         return listcpy
-    if idx > len(my_list):
+    if idx > len(my_list) - 1:
         return listcpy
     else:
         listcpy[idx] = element
