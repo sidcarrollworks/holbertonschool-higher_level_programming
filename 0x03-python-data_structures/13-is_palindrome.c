a7f8ab13a50a3f307c972f2e0ef24da0c586932b
squidcarroll <squidcarroll@gmail.com>
2017-12-13 04:59:35 +0000

    udpate 13

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - finds if linked list is palindrome
+ * @head: pointer to list
+ * Return: 0 if not and 1 if it is
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *finder;
+	unsigned int i;
+	int *number;
+	unsigned int length;
+
+	finder = *head;
+
+	while (finder != NULL)
+		finder = finder->next, length++;
+
+	number = malloc(sizeof(int) * length);
+	if (!number)
+		return (0);
+
+	finder = *head;
+	i = 0;
+	while (finder && i <= length)
+	{
+		number[i] = finder->n;
+		i++, finder = finder->next;
+	}
+	
+	i = 0, length--;
+	while (number[i] == number[length])
+	{
+		if (i == length || i > length)
+		{
+			free(number);
+			return(1);
+		}
+		i++, length--;
+	}
+	free(number);
+	return (0);
+}
