1127728ef2faa4007177bd7fd58c36ccd66b046b
squidcarroll <squidcarroll@gmail.com>
2017-12-15 01:17:55 +0000

    upload not stupid way of doing 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def best_score(my_dict):
     if my_dict:
-        return None
-    num = max(zip(my_dict.values(), my_dict.keys()))
-    return num[1]
+        return max(my_dict.keys())
