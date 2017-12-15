6415de33608815ce000aaf2ba4d5ec6c114d45fd
squidcarroll <squidcarroll@gmail.com>
2017-12-15 00:51:16 +0000

    number 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    num = 0
+    roman = {'M' : 1000, 'D' : 500, 'C' : 100, 'L' : 50, 'X' : 10, 'V' : 5, 'I' : 1}
+
+    if not isinstance(roman_string, str) or roman_string is None:
+        return num
+
+    for x in roman_string:
+        for i in roman.keys():
+            if x == i:
+                num += roman[i]
+    return num
