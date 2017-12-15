14c81a5842f1d31547a7ea144271b8768772b204
squidcarroll <squidcarroll@gmail.com>
2017-12-15 01:16:19 +0000

    fix for 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = my_list.copy()
-    for x in my_list:
+    for i, x in enumerate(my_list):
         if x == search:
-            new_list[x] = replace
+            new_list[i] = replace
     return new_list
