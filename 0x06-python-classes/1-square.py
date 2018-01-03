0be3ba48239085745dce5eb08d5b25368edad92c
squidcarroll <squidcarroll@gmail.com>
2018-01-03 15:20:57 -0800

    fix the documentation

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 class Square:
+    def __init__(self, size=None):
     '''Initialization of instance attributes
         Args:
         size (int): The size of the square
     '''
-    def __init__(self, size=None):
         self.__size = size
