f803ef952e56b3697c7ce39477094ee66cdc595b
squidcarroll <squidcarroll@gmail.com>
2018-01-03 15:19:48 -0800

    document

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 class Square:
-
+    '''Initialization of instance attributes
+        Args:
+        size (int): The size of the square
+    '''
     def __init__(self, size=None):
         self.__size = size
