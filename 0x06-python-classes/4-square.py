a51a49f0c7b8c0ba88d2b375c290851ff2221323
squidcarroll <squidcarroll@gmail.com>
2018-01-04 00:24:19 +0000

    fix error statement

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -3,7 +3,7 @@ class Square:
 
     def __init__(self, size=0):
         if not isinstance(size, int):
-            raise TypeError("size must be a integer")
+            raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
@@ -18,7 +18,7 @@ class Square:
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
-            raise TypeError("size must be a integer")
+            raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
