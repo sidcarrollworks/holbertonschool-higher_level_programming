1263c3f7e459200e6172c487564663e5ecdebf4f
squidcarroll <squidcarroll@gmail.com>
2018-01-04 00:25:11 +0000

    fix error statements

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -9,7 +9,7 @@ class Square:
             size (int): Zero or positve number.
         '''
         if not isinstance(size, int):
-            raise TypeError("size must be a integer")
+            raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
@@ -44,7 +44,7 @@ class Square:
             value (int): Zero or positve number.
         '''
         if not isinstance(value, int):
-            raise TypeError("size must be a integer")
+            raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
