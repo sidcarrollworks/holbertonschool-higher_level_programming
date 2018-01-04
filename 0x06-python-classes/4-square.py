804cd3abd9f479782dd05dbec2efcb59048e75f5
squidcarroll <squidcarroll@gmail.com>
2018-01-04 00:07:03 +0000

    update 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
 class Square:
-	'''
-		define square
-	'''
+
     def __init__(self, size=0):
-		'''Initialization of instance attributes
-            Args:
-            size (int): Zero or positve number.
-        '''
         if not isinstance(size, int):
             raise TypeError("size must be a integer")
         if size < 0:
@@ -15,9 +9,6 @@ class Square:
         self.__size = size
 
     def area(self):
-		'''Calculates the area
-            Return: The current square area.
-        '''
         return self.__size * self.__size
 
     @property
@@ -26,10 +17,6 @@ class Square:
 
     @size.setter
     def size(self, value):
-		'''Updating the private attributes
-            Args:
-            value (int): Zero or positve number.
-        '''
         if not isinstance(value, int):
             raise TypeError("size must be a integer")
         if value < 0:
