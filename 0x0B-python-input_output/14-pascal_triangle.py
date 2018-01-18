ff5f29868261b74affc2dbfc43b25f9e2dae98bf
squidcarroll <squidcarroll@gmail.com>
2018-01-18 02:10:04 +0000

    add 14 with nicks helpp method

--- 0x0B-python-input_output/14-pascal_triangle.py
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -4,4 +4,9 @@ def pascal_triangle(n):
 	new_list = []
 	if n <= 0:
 		return new_list
-
+    arr = [[1]]
+    for i in range(n - 1):
+        arr.append(
+            [x + n for x, n in zip(arr[-1] + [0], [0] + arr[-1])]
+        )
+    return arr
