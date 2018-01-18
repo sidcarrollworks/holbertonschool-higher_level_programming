549e7b1aaf711776616881376909399bfee3d61d
squidcarroll <squidcarroll@gmail.com>
2018-01-18 01:39:50 +0000

    update 2

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def read_lines(filename="", nb_lines=0):
-	with open(filename, 'r') as f:
-		if nb_lines <= 0:
-			lines = f.read()
-			print(lines, end ="")
-		else:
-			lines = [lines for lines in f][:nb_lines]
-			print(lines)
+    with open(filename, 'r') as f:
+        if nb_lines <= 0:
+            print(f.read(), end ="")
+        else:
+            for x in range(0, nb_lines):
+                print(f.readline(), end="")
