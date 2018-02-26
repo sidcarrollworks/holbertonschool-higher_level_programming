eb6786390d28b228b1c69eb2d9399c2c185af3b5
squidcarroll <squidcarroll@gmail.com>
2018-02-26 22:57:32 +0000

    update 15 and 16

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,2 @@
 -- lists all records of the table
-SELECT score, name FROM second_table WHERE name IS NOT NULL
-ORDER BY DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
