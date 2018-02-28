65293fa3a6e8de3a253f5bf9ffdb99241821b6bd
squidcarroll <squidcarroll@gmail.com>
2018-02-28 23:28:48 +0000

    fix quotse

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
 -- creats the database hbtn_0d_2 and the user_0d_2
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON 'hbtn_0d_2' *.* TO user_0d_2@localhost;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON 'hbtn_0d_2' *.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
