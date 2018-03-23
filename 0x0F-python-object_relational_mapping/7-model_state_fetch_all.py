bb4c0eec2a31c69d4a25b73f9fa72f3f8ca283ad
squidcarroll <squidcarroll@gmail.com>
2018-03-23 23:09:41 +0000

    push files

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -3,7 +3,7 @@
 """
 import sys
 from model_state import Base, State
-from sqlalchemy import sessionmaker
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
