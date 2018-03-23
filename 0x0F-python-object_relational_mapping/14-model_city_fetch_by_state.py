173177a7c5641789188682de7c9f74770e4298d9
squidcarroll <squidcarroll@gmail.com>
2018-03-23 20:59:44 +0000

    push files

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    s = Session()
+
+    for x, y in s.query(State, City).join(City).order_by(City.id):
+        print("{}: ({}) {}".format(x.name, y.id, y.name))
+
+    s.close()
