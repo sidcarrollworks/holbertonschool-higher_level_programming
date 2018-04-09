e896baf83e7877a8614a53c145686f97dd020978
squidcarroll <squidcarroll@gmail.com>
2018-04-09 14:21:45 +0000

    push 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -4,10 +4,19 @@
 if __name__ == "__main__":
     import requests
     from sys import argv
+    import sys
     try:
         q = argv[1]
     except:
-        q=""
-    r = requests.get('https://api.github.com/events')
-    r.json()
-    print(r)
+        q = ""
+    r = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
+    try:
+        json = r.json()
+        if len(json) == 0:
+            print("No result")
+        else:
+            ID = json.get('id')
+            NAME = json.get('name')
+            print("[{}] {}".format(ID, NAME))
+    except:
+        print("Not a valid JSON")
