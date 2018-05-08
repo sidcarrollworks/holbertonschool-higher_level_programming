97d2b06c407b9db385ec4bb2951598dc7d339316
squidcarroll <squidcarroll@gmail.com>
2018-05-08 22:05:58 +0000

    push 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,5 +1,8 @@
 #!/usr/bin/node
 let request = require('request');
+request(process.argv[2], (err) => {
+  if (err) throw err;
+});
 request
   .get('http://swapi.co/api/people/18', (err, res, body) => {
     if (err) throw err;
