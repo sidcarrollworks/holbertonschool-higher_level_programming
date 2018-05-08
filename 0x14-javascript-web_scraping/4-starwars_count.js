b8edbcf401b36270e6bf8214ec87fa574a7b1680
squidcarroll <squidcarroll@gmail.com>
2018-05-08 22:18:25 +0000

    fix 4 to use right url

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,10 +1,16 @@
 #!/usr/bin/node
 let request = require('request');
-request(process.argv[2], (err) => {
+request(process.argv[2], (err, res, body) => {
   if (err) throw err;
+  let data = JSON.parse(body);
+  let count = 0;
+  let movies = data.results;
+  for (let i = 0; i < movies.length; i++) {
+    for (let x = 0; x < movies[i].characters.length; x++) {
+      if (movies[i].characters[x] === 'https://swapi.co/api/people/18/') {
+        count += 1;
+      }
+    }
+  }
+  console.log(count);
 });
-request
-  .get('http://swapi.co/api/people/18', (err, res, body) => {
-    if (err) throw err;
-    console.log(JSON.parse(body).films.length);
-  });
